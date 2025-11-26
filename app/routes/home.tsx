2a4c156326e3f8e67d5f5c1af0efccf8d0c50a32
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-11-26 16:22:04 +0300

    impelemented the Authintication logic and test it ... next move is creating UploadForms and File Uploader Component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import {resume} from "react-dom/server";
 import RuesmeCard from "~/components/RuesmeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router";
+import {useEffect} from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,6 +16,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {auth} = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+
     return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar/>
         <section className='main-section'>
